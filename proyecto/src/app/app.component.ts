816b8f7a45df6f491369f46d5726ad57f4a88b98
John Edwin Molina Moscoso <molinje@gmail.com>
2016-12-09 21:09:55 -0500

    prueba

--- proyecto/src/app/app.component.ts
+++ proyecto/src/app/app.component.ts
@@ -28,7 +28,7 @@ import { RegisterPage } from '../pages/register/register';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+  rootPage: any = ProductdetailPage;
   isLogin: boolean = true;
 
 
